74e0e0f4f4d529f54e4c0103fda8654e4c3ae7f1
VINAYCS18 <57659769+VINAYCS18@users.noreply.github.com>
2020-10-01 15:31:31 +0530

    Create 0_calc.sh

---
+++ 0_calc.sh
@@ -0,0 +1,13 @@
+echo "enter two numbers"
+read a
+read b
+echo "addition"
+expr $a + $b
+echo "subtraction"
+expr $a - $b
+echo "product"
+expr $a \* $b
+echo "division"
+expr $a / $b
+echo "modulous"
+expr $a % $b
